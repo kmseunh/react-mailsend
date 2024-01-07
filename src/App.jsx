05029ae37c460c90030496d3d851acc84c4e6ca6
kmseunh <tmdgus8779@gmail.com>
2024-01-07 23:22:23 +0900

    feat: implement ContactList and Chat components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import { useState } from 'react';
+import ContactList from './components/ContactList';
+import Chat from './components/Chat';
+
+const contacts = [
+    { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+    { id: 1, name: 'Alice', email: 'alice@mail.com' },
+    { id: 2, name: 'Bob', email: 'bob@mail.com' },
+];
+
 export default function App() {
-    return <h1>Hello, World!</h1>;
+    const [selectedContact, setSelectedContact] = useState(contacts[0]);
+
+    const handleContactSelect = (contact) => {
+        setSelectedContact(contact);
+    };
+
+    return (
+        <>
+            <ContactList
+                contacts={contacts}
+                selectedContact={selectedContact}
+                onSelect={handleContactSelect}
+            />
+            <Chat key={selectedContact.id} contact={selectedContact} />
+        </>
+    );
 }
